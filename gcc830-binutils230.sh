bea5416289b5b6afc0efcea55840436ce6787bde
RMuskovets <rmuskovets@gmail.com>
2020-02-29 11:26:20 +0200

    Add PUSHD/POPD

--- gcc830-binutils230.sh
+++ gcc830-binutils230.sh
@@ -7,16 +7,18 @@ export PATH="$PREFIX/bin:$PATH"
 
 # Install Binutils
 mkdir build-binutils
-cd build-binutils
+pushd build-binutils
 ../binutils-2.30/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
 make
 make install
+popd
 
 # Install GCC
 # The $PREFIX/bin dir _must_ be in the PATH. We did that above.
 which -- $TARGET-as || echo $TARGET-as is not in the PATH
 mkdir build-gcc
-cd build-gcc
+pushd build-gcc
 ../gcc-8.3.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 make
 # make install
+popd
