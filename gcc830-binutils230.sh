db02e732b5d7d3ef65525cabe4da872613b397ff
RMuskovets <rmuskovets@gmail.com>
2020-02-29 09:55:57 +0200

    Update gcc830-binutils230.sh

--- gcc830-binutils230.sh
+++ gcc830-binutils230.sh
@@ -18,7 +18,5 @@ which -- $TARGET-as || echo $TARGET-as is not in the PATH
 mkdir build-gcc
 cd build-gcc
 ../gcc-8.3.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
-make all-gcc
-make all-target-libgcc
-make install-gcc
-make install-target-libgcc
+make
+make install
